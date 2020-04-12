a416c0432ea10fb2a41594ce100b6f2d73eae5a0
Tugay Cem Evci <46964249+tugayevci@users.noreply.github.com>
2020-04-12 16:44:43 +0300

    Initial commit

---
+++ Mad Birds/Assets/Bird.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+
+    Vector3 _initialPosition;
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (transform.position.y > 10)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 500);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+    }
+}
